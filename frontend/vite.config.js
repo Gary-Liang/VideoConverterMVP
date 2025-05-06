cfa4b18b49885527e0432c3c55f0dbe53bdc2f5d
Gary Liang <r4dsgary@yahoo.com>
2025-05-05 23:36:53 -0500

    revised vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
+export default defineConfig({
   css: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
-}
+  base: '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+});
